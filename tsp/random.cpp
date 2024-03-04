1d828ec44aa4b8fc320f6589caf2ad089acbe7b5
German Perov <ggmusya@gmail.com>
2024-03-04 23:40:44 +0300

    Use absl::flags

--- tsp/random.cpp
+++ tsp/random.cpp
@@ -1,3 +1,16 @@
-int main() {
-    return 0;
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+
+#include <iostream>
+#include <string>
+
+ABSL_FLAG(std::string, input, {}, "Input file");
+ABSL_FLAG(std::string, output, {}, "Output file");
+
+int main(int argc, char **argv) {
+  absl::ParseCommandLine(argc, argv);
+  std::string input = absl::GetFlag(FLAGS_input);
+  std::string output = absl::GetFlag(FLAGS_output);
+  std::cout << input << ' ' << output << std::endl;
+  return 0;
 }
